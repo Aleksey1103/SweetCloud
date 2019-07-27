969a06b315976950391989f96fe7bcbb897f682f
Aleksey1103 <47826423+Aleksey1103@users.noreply.github.com>
2019-07-27 13:53:50 +0300

    updating

--- js/main.js
+++ js/main.js
@@ -332,8 +332,6 @@ $(document).ready(function(){
 
         event.preventDefault();
 
-        $(".modal-btn").css("background-color", "#000");
-
         $(".btn-close").show();
         $(".modal-menu").css("display", "flex").animate({top: "0px"}, 500);
 
